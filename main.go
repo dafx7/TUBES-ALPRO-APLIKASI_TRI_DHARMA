4ab2521c1deb02415cdde6090d4c61be5b10315f
Hafiz <hfzfahrial@gmail.com>
2024-05-21 22:55:01 +0700

    menyelesaikan menu_utama dan menambahkan tipe data struktur dan array

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+const NMAX int = 30
+
+type PPM struct {
+	ketua, prodi, judul, sumber_dana, luaran, tahun_kegiatan string
+	anggota                                                  [4]string
+}
+
+type arrPPM [NMAX]PPM
+
 func main() {
 	fmt.Println("Selamat datang di aplikasi Tri Dharma Perguruan Tinggi.")
 	menu_utama()
@@ -24,8 +33,39 @@ func menu_utama() {
 	tulisan_menu()
 	fmt.Scan(&pilihan)
 	for pilihan != 6 {
+		if pilihan == 1 {
+			tambah_data()
+		} else if pilihan == 2 {
+			edit_data()
+		} else if pilihan == 3 {
+			hapus_data()
+		} else if pilihan == 4 {
+			tampilkan_data()
+		} else if pilihan == 5 {
+			urutkan_data()
+		}
 		tulisan_menu()
 		fmt.Scan(&pilihan)
 	}
 	fmt.Print("Terimakasih telah menggunakan aplikasi Tri Dharma Perguruan Tinggi.")
+}
+
+func tambah_data() {
+
+}
+
+func edit_data() {
+
+}
+
+func hapus_data() {
+
+}
+
+func tampilkan_data() {
+
+}
+
+func urutkan_data() {
+
 }
