1b6be5eda221cd4cea6d25266966f8973f986059
Dapnek <ahmaddaffaamd@gmail.com>
2024-05-21 22:43:19 +0700

    Memulai pengembangan menu_utama

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("abcs")
+	fmt.Println("Selamat datang di aplikasi Tri Dharma Perguruan Tinggi.")
+	menu_utama()
+}
+
+func tulisan_menu() {
+	fmt.Println("1. Menambahkan Data")
+	fmt.Println("2. Edit Data")
+	fmt.Println("3. Hapus Data")
+	fmt.Println("4. Tampilkan Data")
+	fmt.Println("5. Urutkan Data")
+	fmt.Println("6. Keluar")
+
+	fmt.Print("Input pilihan: ")
+}
+
+func menu_utama() {
+	var pilihan int
+
+	tulisan_menu()
+	fmt.Scan(&pilihan)
+	for pilihan != 6 {
+		tulisan_menu()
+		fmt.Scan(&pilihan)
+	}
+	fmt.Print("Terimakasih telah menggunakan aplikasi Tri Dharma Perguruan Tinggi.")
 }
